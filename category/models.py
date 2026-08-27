ecd1f392113770ac8dc3661a606daea915feafa1
Snigdha <your-email@gmail.com>
2026-08-27 16:27:18 +0530

    this is my first commit

--- category/models.py
+++ category/models.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
-    category_name = models.CharField(max_length=50, unique=True)
+    category_name = models.CharField(max_length=50, unique=True )
     slug = models.SlugField(max_length=50, unique=True)
     description = models.TextField(max_length=255, blank= True)
     image= models.ImageField(upload_to='photos/categories',blank=True)
