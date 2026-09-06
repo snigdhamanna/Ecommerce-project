c63b09a4c651ab21b4a4cbbbdd2d446d15637c6e
Snigdha <your-email@gmail.com>
2026-09-07 00:24:13 +0530

    Completed payment system

--- orders/views.py
+++ orders/views.py
@@ -5,11 +5,71 @@ from .forms import OrderForm
 from .models import Order, Payment, OrderProduct
 from django.contrib import messages
 import datetime
-
+import json
 
 
 def payments(request):
-    return render(request, 'orders/payments.html')
+    body = json.loads(request.body)
+    order = Order.objects.get(user=request.user, is_ordered=False, order_number=body['order'])
+    # Store transaction details inside Payment model
+    payment = Payment(
+        user = request.user,
+        payment_id = body['transID'],
+        payment_method = body['payment_method'],
+        amount_paid = order.order_total,
+        status = body['status'],
+    )
+    payment.save()
+
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
+
+    # Move the cart items to Order Product table
+    cart_items = CartItem.objects.filter(user=request.user)
+
+    for item in cart_items:
+        orderproduct = OrderProduct()
+        orderproduct.order_id = order.id
+        orderproduct.payment = payment
+        orderproduct.user_id = request.user.id
+        orderproduct.product_id = item.product_id
+        orderproduct.quantity = item.quantity
+        orderproduct.product_price = item.product.price
+        orderproduct.ordered = True
+        orderproduct.save()
+
+        cart_item = CartItem.objects.get(id=item.id)
+        product_variation = cart_item.variations.all()
+        orderproduct = OrderProduct.objects.get(id=orderproduct.id)
+        orderproduct.variations.set(product_variation)
+        orderproduct.save()
+
+
+    #     # Reduce the quantity of the sold products
+    #     product = Product.objects.get(id=item.product_id)
+    #     product.stock -= item.quantity
+    #     product.save()
+
+    # # Clear cart
+    # CartItem.objects.filter(user=request.user).delete()
+
+    # # Send order recieved email to customer
+    # mail_subject = 'Thank you for your order!'
+    # message = render_to_string('orders/order_recieved_email.html', {
+    #     'user': request.user,
+    #     'order': order,
+    # })
+    # to_email = request.user.email
+    # send_email = EmailMessage(mail_subject, message, to=[to_email])
+    # send_email.send()
+
+    # data = {
+    #     'order_number': order.order_number,
+    #     'transID': payment.payment_id,
+    # }
+    # return JsonResponse(data)
+    return render(request,'orders/payments.html')
 
 
 
